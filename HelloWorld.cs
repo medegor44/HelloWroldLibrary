18886b2453226a3cd46ac3b41d3a4afeba6198fa
Egor Medvedev <Egor.Medvedev@confirmit.com>
2021-10-29 12:30:01 +0300

    Initial commit

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWroldLibrary
+{
+    public class HelloWorld
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
